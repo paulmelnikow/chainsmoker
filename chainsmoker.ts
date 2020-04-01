20fc145254515c40fdd3525e956723f6f453be8e
Paul Melnikow <email@paulmelnikow.com>
2020-04-01 13:05:40 -0400

    Update config + reformat

--- chainsmoker.ts
+++ chainsmoker.ts
@@ -20,8 +20,8 @@ export default function chainsmoker<T>(
 ): Chainsmoker<T> {
   function matchPatterns(patterns: Pattern[]): KeyedPaths<T> {
     return mapValues(keyedPaths, (paths: Path[]) => {
-      const excludePatterns = patterns.filter(p => isExclude(p))
-      const includePatterns = patterns.filter(p => !isExclude(p))
+      const excludePatterns = patterns.filter((p) => isExclude(p))
+      const includePatterns = patterns.filter((p) => !isExclude(p))
 
       const included = includePatterns.reduce(
         (accum, pattern) => accum.concat(micromatch.match(paths, pattern)),
